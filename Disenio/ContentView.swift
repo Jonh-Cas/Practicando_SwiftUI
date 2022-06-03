9c8dd11fde20c6a75b06d24e13e91db7a02583ae
Jonathan Casillas Mora <react.jonathan.casillas@gmail.com>
2022-06-02 21:39:40 -0500

    Se termino el proyecto de practica de SwirfUI

--- Disenio/ContentView.swift
+++ Disenio/ContentView.swift
@@ -32,6 +32,52 @@
  Ejemplo
  Image(systemName: "gamecontroller").font(.system(.size: 30 ))
  
+Modificadores
+ 
+ creas una nueva vista para un componente
+ Ejemplo:
+ 
+ struct boton : ViewModifier{
+     
+ var color: Color
+ 
+ func body(content: Content) -> some View {
+ content
+ .font(.system(size: 60))
+ .background(color)
+ .foregroundColor(.blue)
+ .clipShape(Circle())
+ .padding()
+ }
+ }
+ 
+ colorcacion
+ 
+ 
+ Image(systemName: "phone.fill")
+ .modifier(boton(color: .white))
+ 
+ 
+ COMO HACER LLAMADAS Y COMO MANDAR MENSAJE
+ 
+ Ejemplo
+ 
+ let numero = "5555555555"
+ let mensaje = "Hola como estas?"
+ 
+ 
+ func sendMessage (){
+     let sms = "sms: \(numero)&body=\(mensaje)"
+     guard let stringSMS = sms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+     else{ return }
+     UIApplication.shared.open(URL.init(string: stringSMS)!, options: [:], completionHandler: nil)
+ }
+ 
+ func sendCall (){
+     guard let number = URL(string: "tel://\(numero)" ) else { return }
+     UIApplication.shared.open(number)
+ }
+ 
  
  */
 
@@ -58,9 +104,202 @@ struct ContentView: View {
 }
 
 struct compactDesing: View {
+    
+    let numero = "5555555555"
+    let mensaje = "Hola como estas?"
+    
+    func sendMessage (){
+        let sms = "sms: \(numero)&body=\(mensaje)"
+        guard let stringSMS = sms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        else{ return }
+        UIApplication.shared.open(URL.init(string: stringSMS)!, options: [:], completionHandler: nil)
+    }
+    
+    func sendCall (){
+        guard let number = URL(string: "tel://\(numero)" ) else { return }
+        UIApplication.shared.open(number)
+    }
+    
     var body: some View {
         ZStack(alignment: .top){
             Color.indigo.edgesIgnoringSafeArea(.all)
+            ScrollView {
+                
+                
+                VStack(alignment: .center){
+                    
+                    let texto: Text = Text("Practicando con Swift")
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+
+                    texto
+                        .bold()
+                        .font(.system(size: 40))
+                        .italic()
+                        .underline()
+                        .padding()
+                    
+                    HStack{
+                        Text("Texto ")
+                            .font(.system(size: 40))
+                            .foregroundColor(.white)
+                            .italic()
+                        
+                        Text("Iconos ")
+                            .font(.system(size: 40))
+                            .foregroundColor(.white)
+                            .italic()
+                    }.padding()
+                    
+                    
+                    HStack{
+                        Text("Poniendo una imagen ")
+                            .font(.system(size: 20))
+                            .foregroundColor(.white)
+                            .italic()
+                        
+                        Image("pokemon")
+                            .resizable()
+                            .frame(width: 100, height: 100, alignment: .leading)
+                    }.padding()
+                    
+                    
+                    HStack{
+                        
+                        Button(action: {
+                            sendCall()
+                        }){
+
+                            Image(systemName: "phone.fill")
+                                .modifier(boton(color: .white))
+                        }
+                    }
+                    
+                    
+                    
+                    HStack(alignment: .center){
+                        
+                        
+                        Button(action: {
+                            sendMessage()
+                        }){
+                            Text("bubble.left.fill")
+                                .foregroundColor(.white)
+                                .italic()
+                                .font(.system(size: 30))
+                            Image(systemName: "message")
+                                .modifier(boton(color: .white))
+                        }
+                        
+                    }
+                    
+                    HStack(alignment: .center, spacing: 150){
+                        
+                        Button(action: {
+                            
+                        }){
+                            
+                            Text("gamecontroller")
+                                .foregroundColor(.white)
+                                .italic()
+                                .font(.system(size: 30))
+                            Image(systemName: "gamecontroller" )
+                                .font(.system(size: 60))
+                                .background(.indigo)
+                                .foregroundColor(.black)
+                            
+                        }
+                    }
+                    HStack(alignment: .center, spacing: 150){
+                        
+                        Button(action: {
+                            
+                        }){
+                            
+                            Text("house")
+                                .font(.largeTitle)
+                                .foregroundColor(.white)
+                                .italic()
+                            Image(systemName: "house.fill")
+                                .font(.system(size: 60))
+                                .foregroundColor(.green)
+                        }
+                    }
+                    
+                }.padding(.all)
+                
+                HStack(alignment: .center, spacing: 150){
+                    
+                    Button(action:  {
+                        
+                    }){
+                        
+                        Text("keyboard")
+                            .foregroundColor(.white)
+                            .font(.system(size: 30))
+                        Image(systemName: "keyboard")
+                            .font(.system(size: 60))
+                            .foregroundColor(.black)
+                    }
+                    
+                }.padding(.all)
+                
+                HStack(alignment: .center, spacing: 150){
+                    
+                    Button(action:  {
+                        
+                    }){
+                        
+                        Text("car.fill")
+                            .foregroundColor(.white)
+                            .font(.system(size: 30))
+                        Image(systemName: "car.fill")
+                            .font(.system(size: 60))
+                            .foregroundColor(.white)
+                    }
+                    
+                }.padding(.all)
+                
+                HStack(alignment: .center, spacing: 150){
+                    
+                    Button(action:  {
+                        
+                    }){
+                        
+                        Text("airplane")
+                            .foregroundColor(.white)
+                            .font(.system(size: 30))
+                        Image(systemName: "airplane")
+                            .font(.system(size: 60))
+                            .foregroundColor(.gray)
+                    }
+                    
+                }.padding(.all)
+                
+            }
+        }
+        
+    }
+}
+
+struct boton : ViewModifier{
+    
+    var color: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 60))
+            .background(color)
+            .foregroundColor(.blue)
+            .clipShape(Circle())
+            .padding()
+    }
+}
+
+
+struct regularDesing: View {
+    var body: some View {
+        ZStack{
+            Color.red.edgesIgnoringSafeArea(.all)
             ScrollView {
                 
                 
@@ -111,9 +350,10 @@ struct compactDesing: View {
                                 .italic()
                                 .font(.system(size: 30))
                             Image(systemName: "phone.fill")
+                                .padding(.all)
                                 .font(.system(size: 60))
-                                .background(Color.indigo)
-                                .foregroundColor(.orange)
+                                .background(.white)
+                                .foregroundColor(.green)
                         }.frame( alignment: .leading )
                     }.padding(.all)
                     
@@ -218,93 +458,6 @@ struct compactDesing: View {
                     }
                     
                 }.padding(.all)
-                
-            }
-        }
-        
-    }
-}
-
-struct regularDesing: View {
-    var body: some View {
-        ZStack(alignment: .top){
-            Color.blue.edgesIgnoringSafeArea(.all)
-            ScrollView {
-                
-                
-                VStack(alignment: .center){
-                    Image("pokemon")
-                        .resizable()
-                        .frame(width: 100, height: 100, alignment: .top)
-                        .clipShape(Circle())
-                    
-                    let texto: Text = Text("Pokemones")
-                        .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                        
-                        
-                    texto
-                        .bold()
-                        .font(.system(size: 40))
-                    
-                    HStack{
-                        texto
-                            .foregroundColor(.indigo)
-                            .italic()
-                        
-                        Image("pokemon")
-                            .resizable()
-                            .frame(width: 100, height: 100, alignment: .leading)
-                            .clipShape(Circle())
-                    }
-                    HStack(alignment: .center ){
-                        
-                        texto
-                            .foregroundColor(.indigo)
-                            .italic()
-                        Image("pokemon")
-                            .resizable()
-                            .frame(width: 100, height: 100, alignment: .center)
-                            .clipShape(Circle())
-                        
-                    }
-                    HStack(alignment: .center, spacing: 150){
-                        Text("Venasaur")
-                            .foregroundColor(.indigo)
-                            .italic()
-                        Image("pokemon")
-                            .resizable()
-                            .frame(width: 100, height: 100, alignment: .center)
-                            .clipShape(Circle())
-                    }
-                    HStack(alignment: .center, spacing: 150){
-                        Text("Charmander")
-                            .foregroundColor(.indigo)
-                            .italic()
-                        
-                        Image("pokemon")
-                            .resizable()
-                            .frame(width: 100, height: 100, alignment: .center)
-                            .clipShape(Circle())
-                    }
-                    HStack(alignment: .center, spacing: 150){
-                        Text("Charizard")
-                            .foregroundColor(.indigo)
-                        Image("pokemon")
-                            .resizable()
-                            .frame(width: 100, height: 100, alignment: .center)
-                            .clipShape(Circle())
-                    }
-                    HStack(alignment: .center, spacing: 150){
-                        Text("Warturtle")
-                            .foregroundColor(.indigo)
-                        Image("pokemon")
-                            .resizable()
-                            .frame(width: 100, height: 100, alignment: .center)
-                            .clipShape(Circle())
-                    }
-                    
-                }
-                .padding(.all)
                 
             }
         }
